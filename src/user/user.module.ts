1579cb1fcd322413d7b53c2c14a80e3c10d59ab6
JibAb96 <j.abshir96@gmail.com>
2025-03-17 17:32:22 +0100

    Import typeorm module inorder to feature the User entity. Cofigure the UserModule to incorporate the CurrentUserMiddleware.

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
-import { UserController } from './user.controller';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { UsersService } from './user.service';
+import { UsersController } from './user.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
 import { AuthService } from './auth.service';
-
+import { CurrentUserMiddleware } from './middlewares/current-user.middleware';
 @Module({
-  providers: [UserService, AuthService],
-  controllers: [UserController]
+  imports: [TypeOrmModule.forFeature([User])],
+  providers: [UsersService, AuthService],
+  controllers: [UsersController],
 })
-export class UserModule {}
+export class UserModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(CurrentUserMiddleware).forRoutes('*');
+  }
+}
