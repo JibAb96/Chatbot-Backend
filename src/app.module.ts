1d2aeace5fbd5bd6968fbb6104c8ba8ca863a624
JibAb96 <j.abshir96@gmail.com>
2025-03-17 17:36:44 +0100

    Import Config Module to make use of config files for database configuration. Also import TypeOrm Module to configure the datebase. Provide Validation Pipe dependency globally to allow for validation decoratoras in the dto files. Configure App module to apply the cookie session middleware to keep track of signed in user.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,51 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, ValidationPipe } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { AuthService } from './auth/auth.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/user.entity';
+import { APP_PIPE } from '@nestjs/core';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UserModule } from "./user/user.module";
+const cookieSession = require('cookie-session');
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.development`,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory(config: ConfigService) {
+        return {
+          type: 'sqlite',
+          database: config.get<string>('DB_NAME'),
+          synchronize: true,
+          entities: [User],
+        };
+      },
+    }),
+    
+    UserModule,
+  ],
   controllers: [AppController],
-  providers: [AppService, AuthService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+      }),
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(
+        cookieSession({
+          keys: ['lkdjnls'],
+        }),
+      )
+      .forRoutes('*');
+  }
+}
