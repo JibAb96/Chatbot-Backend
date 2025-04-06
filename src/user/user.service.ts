a81862c32995752be392ca19581df4f153d5fef4
JibAb96 <j.abshir96@gmail.com>
2025-04-06 15:54:30 +0200

    Change create method in UsersService to receive object including first name and last name property. Fix the update method to hash passwords befpre updating the database.

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,12 +1,17 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
-import { User } from "./user.entity"
+import { User } from './user.entity';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
+
+const scrypt = promisify(_scrypt);
 @Injectable()
 export class UsersService {
   constructor(@InjectRepository(User) private repo: Repository<User>) {}
-  create(email: string, password: string) {
-    const user = this.repo.create({ email, password });
+  create(formData: CreateUserDto) {
+    const user = this.repo.create(formData);
     return this.repo.save(user);
   }
   findOne(id: number) {
@@ -20,10 +25,19 @@ export class UsersService {
     return this.repo.find({ where: { email } });
   }
 
-  async update(id: number, attrs: Partial<User>) {
+  async update(id: number, attrs: Partial<CreateUserDto>) {
     const user = await this.findOne(id);
     if (!user) {
       throw new NotFoundException('user not found');
+    }
+    if (
+      Object.hasOwn(attrs, 'password') &&
+      typeof attrs.password === 'string'
+    ) {
+      const salt = randomBytes(8).toString('hex');
+      const hash = (await scrypt(attrs.password, salt, 32)) as Buffer;
+      const result = salt + '.' + hash.toString('hex');
+      attrs.password = result;
     }
     Object.assign(user, attrs);
     return this.repo.save(user);
