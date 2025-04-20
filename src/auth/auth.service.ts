39d95f379428a9033c9aa3e7c38ad08a9070ea43
JibAb96 <j.abshir96@gmail.com>
2025-04-20 16:17:49 +0200

    Create auth service file to handle buisness logic involving supabase auth database.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { AuthRepository } from './auth.repository';
+import { RegisterAuthDto } from './dtos/register-auth.dto';
+import { UpdateAuthDto } from './dtos/update-auth.dto';
+import { SignInAuthDto } from './dtos/login-auth.dto';
+import { UpdateAuthUserResponse } from './interfaces/update-authuser-responser.interface';
+import { AuthResponse } from './interfaces/auth-response.interface';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly authRepository: AuthRepository) {}
+
+  async register(body: RegisterAuthDto): Promise<AuthResponse> {
+    const authData = await this.authRepository.register(
+      body.email,
+      body.password,
+    );
+    return authData;
+  }
+
+  async login(authDto: SignInAuthDto): Promise<AuthResponse> {
+    const authData = await this.authRepository.login(
+      authDto.email,
+      authDto.password,
+    );
+    return authData;
+  }
+
+  async update(updateAuthDto: UpdateAuthDto): Promise<UpdateAuthUserResponse> {
+    const data = await this.authRepository.updateUser(updateAuthDto);
+    return data;
+  }
+
+  async deleteUser(id: string): Promise<boolean> {
+    return await this.authRepository.deleteUser(id);
+  }
+}
