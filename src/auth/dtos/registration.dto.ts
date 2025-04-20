f5fcb0d005e364de5efd4780ce51598d1d59c475
JibAb96 <j.abshir96@gmail.com>
2025-04-20 16:12:34 +0200

    Create dto for user registration form.

---
+++ src/auth/dtos/registration.dto.ts
@@ -0,0 +1,27 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class RegisterDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8, { message: 'Password must be at least 8 characters long' })
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/, {
+    message:
+      'Password must contain at least one uppercase letter, one lowercase letter, and one number',
+  })
+  password: string;
+
+  @MinLength(2)
+  @MaxLength(50)
+  username: string;
+}
